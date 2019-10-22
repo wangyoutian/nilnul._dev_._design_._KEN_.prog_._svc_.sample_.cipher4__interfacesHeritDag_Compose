4f3ecde8be5329b1adced59840b8535d699d2251
wangyoutian <wangyoutian@nilnul.com>
2019-10-22 22:58:35 +0800

    add and commit all for backup

---
+++ LIB/Aes.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    public class Aes : CipherI
+    {
+        public Encipher Encipher
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public Decipher Decipher
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+    }
+}
---
+++ LIB/Encipher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    /// <summary>
+    /// aes 
+    /// </summary>
+    public class Encipher : EncipherI
+    {
+        public byte[] encipher(byte[] bytes)
+        {
+
+
+            throw new NotImplementedException();
+        }
+    }
+
+}
